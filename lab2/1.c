7b435cea74d4fc33fd1fa4a7d6509f6512cc716a
李婧祎 <lijingyi@lijingyideMacBook-Pro.local>
2019-03-20 15:39:51 +0800

    reorder

---
+++ lab2/1.c
@@ -0,0 +1,16 @@
+#include <unistd.h> 
+#include <stdio.h>
+ int main (){
+pid_t fpid; 
+int count=0;
+fpid=fork(); 
+if (fpid < 0)
+printf("error in fork!"); 
+else if (fpid == 0) {
+execl("/usr/bin/vi","vi",NULL); 
+}
+else { for(;;){}
+}
+return 0;
+ }
+
